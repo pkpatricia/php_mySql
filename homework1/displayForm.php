54cf5a308fa8386725406cb5f8ee9f59ea304ce8
pkpatricia <pk.patricia@gmail.com>
2015-02-21 06:47:00 -0600

    initial commit

---
+++ homework1/displayForm.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Homework 1 results</title>
+</head>
+<body>
+
+	<?php
+		print ("First Name: " . $_POST["fname"] . "<br>");
+		print ("Last Name: " . $_POST["lname"] . "<br>");
+	
+	?>
+
+</body>
+</html>
